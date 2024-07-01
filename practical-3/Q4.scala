6798c126b3db2bdad8bb6333cb6c6a3ab29f9990
theshawa <mrclocktd@gmail.com>
2024-07-02 02:00:04 +0530

    practical-3

--- practical-3/Q4.scala
+++ practical-3/Q4.scala
@@ -1,10 +1,13 @@
 object Q4 extends App{
   def sum_of_list(int_list:List[Int]):Int = int_list match{
-    case lst if lst.length==1 => int_list(0);
-    case _ => sum_of_list(int_list.tail) + int_list.head;
+    case lst if lst.isEmpty => 0;
+    case _ => sum_of_list(int_list.tail) + (int_list.head match{
+      case x if x%2==0 => x;
+      case _ => 0;
+    });
   };
 
   val int_list = List(1,2,3,4,5);
   val sum = sum_of_list(int_list);
-  println(s"sum of $int_list is $sum");
+  println(s"sum of even in $int_list is $sum");
 }
